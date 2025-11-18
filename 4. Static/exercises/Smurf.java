ef6fa131c2b9443b763e44d566dba6caf4c34317
sarah <sara.bragga01@gmail.com>
2025-11-18 08:23:58 -0300

    exercise - static variables

--- 4. Static/exercises/Smurf.java
+++ 4. Static/exercises/Smurf.java
@@ -1,11 +1,20 @@
 package exercises;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Smurf {
 
 	private String name;
+	private static Set<String> usedNames = new HashSet<>();
 
 	public static Smurf createSmurf(String name) {
+		if (usedNames.contains(name)) {
+			System.out.println("Smurf with name " + name + " already exists! Choose a different name.");
+			return null;
+		}
 		System.out.println("Creating " + name + " Smurf");
+		usedNames.add(name);
 		return new Smurf(name);
 	}
 
